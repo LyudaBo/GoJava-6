b30cb0f62a7ebc8906da58e88af42efc4ff97721
LyudaBo <lyuda-boyun@ukr.net>
2017-02-12 16:32:34 +0200

    Task3.1

---
+++ Module3/3.1/src/Bird.java
@@ -0,0 +1,10 @@
+
+public class Bird {
+    public void sing(String[] birdSay) {
+        for (int i = 0; i < birdSay.length; i++) {
+            System.out.println(birdSay[i] + " ");
+    }
+
+    }
+}
+
---
+++ Module3/3.1/src/ChangeMethod.java
@@ -0,0 +1,10 @@
+
+public class ChangeMethod {
+    public static void main(String args[])
+    {
+        String [] birdSay ={"Im singing", "Im flying", "Im walking", "Im Bird"};
+        Bird  bird = new Bird();
+        bird.sing(birdSay);
+
+    }
+}
