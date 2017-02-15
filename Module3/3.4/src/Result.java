489394273ab82f9bf26f918ef548a6919c690464
LyudaBo <lyuda-boyun@ukr.net>
2017-02-16 00:46:23 +0200

    Task3.4

---
+++ Module3/3.4/src/Result.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Людмила on 16.02.2017.
+ */
+public class Result {
+    public static void main(String[] args) {
+        String companyName = "Rolex";
+        System.out.println(companyName.length());
+    }
+}
