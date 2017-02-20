9ee2146e1f59b667832c6e60518994c91e66fc38
LyudaBo <lyuda-boyun@ukr.net>
2017-02-20 12:06:37 +0200

    Task 4.2

---
+++ Module4/4.2/src/Bank.java
@@ -0,0 +1,18 @@
+
+
+
+public class Bank {
+    private long id;
+    private String bankCountry;
+    private Currency currency;
+    private int numberOfEmployees;
+    private double avrSalaryOfEmployee;
+    private long rating;
+    private long totalCapital;
+
+    public enum Currency {
+        USD,
+        EUR
+    }
+
+}
