8aa9e4673d096c20b1c77dd0dc94a8990dc5e7cf
LyudaBo <lyuda-boyun@ukr.net>
2017-04-17 20:12:44 +0300

    не готово

---
+++ Module10/10.5/src/Class2.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Людмила on 17.04.2017.
+ */
+public class Class2 extends Exception {
+}
---
+++ Module10/10.5/src/Class3.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Людмила on 17.04.2017.
+ */
+public class Class3 extends Exception{
+}
