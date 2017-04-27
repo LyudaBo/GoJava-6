03718d141436c3dba0b488d83527f285869a7fe6
LyudaBo <lyuda-boyun@ukr.net>
2017-04-27 17:17:08 +0300

    Task 11.0

---
+++ Module11/11.0/src/Files.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Created by Людмила on 20.04.2017.
+ * Задание 1
+ * Создать метод, который заменяет слова в файле (File) и возвращает строку (String) с заменёнными значениями.
+ * String replacer(Map map)
+ * <p>
+ * Задание 2
+ * Создать метод, который заменяет слова в файле (File) и переписать его содержание с новыми значениями.
+ * File fileContentReplacer(Map map)
+ * <p>
+ * Задание 3
+ * Создать метод, который заменяет слова в файле (File), и вывести результат в существующий контент файла.
+ * File fileContentMerger(Map<String, String> map)
+ * <p>
+ * Задание 4
+ * Проверить, содержит ли файл конкретное слово. Вывести 0, если нет. Вывести номер n, который эквивалентен числу появлений этого слова в файле.
+ * int checkWord(String word)
+ * Написать это задание с обычным try и try-with-resources(две версии).
+ */
+public class Files {
+    public static String replacer(Map<String, String> map) throws IOException {
+    }
+}
---
+++ Module11/11.0/src/Main.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+/**
+ * Created by Людмила on 20.04.2017.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
+
+
+
